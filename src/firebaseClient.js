da8f562ee4a5cc214c9017aa5f886a04373669e9
chasepope47 <chasepope47@gmail.com>
2025-12-15 16:44:12 +0000

    removing analytics from firebase client

--- src/firebaseClient.js
+++ src/firebaseClient.js
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -19,7 +18,6 @@ const app = initializeApp(firebaseConfig);
 
 // Firebase services
 export const auth = getAuth(app);
-export const db = getFirestore(app);
 
 // Analytics (safe in prod; optional in dev)
 export const analytics =
